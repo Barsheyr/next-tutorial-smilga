72d7e46ad71026aefa4b7c001627f38e0e9737b6
Bashir Amokomowo <barsheyr@gmail.com>
2024-03-07 09:40:57 +0100

    First commit

---
+++ app/(dashboard)/auth/[...sign-in]/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignInpage = ({ params }) => {
+  return <div> SignInpage </div>;
+};
+
+export default SignInpage;
---
+++ app/about/info/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const AboutInfoPage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">About InfoPage </h1>
+      <Link href="/"> Home Page </Link>
+    </div>
+  );
+};
+
+export default AboutInfoPage;
